fdbcb5a9d0d42c3fc3b7d320b06bacbfd28e551b
THEDEATHBOT <jafroth@gmail.com>
2018-03-08 21:33:51 -0800

    Condition check that game data string is not null

--- src/org/usfirst/frc/team997/robot/commands/AutoCenterSwitchDelivery.java
+++ src/org/usfirst/frc/team997/robot/commands/AutoCenterSwitchDelivery.java
@@ -10,36 +10,38 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoCenterSwitchDelivery extends CommandGroup {
 
     public AutoCenterSwitchDelivery() {
-    	addParallel(new FlopDown());
-    	addParallel(new ElevatorToHeight(RobotMap.Values.elevatorSafeDriveHeight));
-    	//addParallel(new ElevatorToHeight(RobotMap.Values.elevatorSwitchHeight));
-    	//Drive forward to avoid exhchange zone. Always 2 feet
-    	addSequential(new PDriveToDistance(2 * RobotMap.Values.ticksPerFoot)); 
+    	if(!Robot.getGameData().isEmpty()) {
+    		addParallel(new FlopDown());
+    		addParallel(new ElevatorToHeight(RobotMap.Values.elevatorSafeDriveHeight));
+    		//addParallel(new ElevatorToHeight(RobotMap.Values.elevatorSwitchHeight));
+    		//Drive forward to avoid exhchange zone. Always 2 feet
+    		addSequential(new PDriveToDistance(2 * RobotMap.Values.ticksPerFoot)); 
     	
-    	if(Robot.getGameData().charAt(0) == 'L') {
-    		addSequential(new PDriveToAngle(-65)); //Turn left to face switch.
-    		addSequential(new PDriveToDistance(3.887 * RobotMap.Values.ticksPerFoot)); //Diagonal length towards our switch. Prev value: 5.5
-    		addSequential(new PDriveToAngle(60)); //Turn right to face straight again.
-    		addSequential(new ElevatorToHeight(RobotMap.Values.elevatorSwitchHeight));
-    		addSequential(new PDriveToDistance((RobotMap.Values.autoLeftSwitchTotal - 5.887) * RobotMap.Values.ticksPerFoot)); //Drive to reach switch for cube delivery. Prev value: 4.15
-    		//PRETTY ACCURATE NOW!!:^)
-    		//When our side of the switch is on the left, this will deliver the cube to
-    		//that side.
+    		if(Robot.getGameData().charAt(0) == 'L') {
+    			addSequential(new PDriveToAngle(-65)); //Turn left to face switch.
+    			addSequential(new PDriveToDistance(3.887 * RobotMap.Values.ticksPerFoot)); //Diagonal length towards our switch. Prev value: 5.5
+    			addSequential(new PDriveToAngle(60)); //Turn right to face straight again.
+    			addSequential(new ElevatorToHeight(RobotMap.Values.elevatorSwitchHeight));
+    			addSequential(new PDriveToDistance((RobotMap.Values.autoLeftSwitchTotal - 5.887) * RobotMap.Values.ticksPerFoot)); //Drive to reach switch for cube delivery. Prev value: 4.15
+    			//PRETTY ACCURATE NOW!!:^)
+    			//When our side of the switch is on the left, this will deliver the cube to
+    			//that side.
+    		}
+    		else {
+    			addSequential(new PDriveToAngle(65)); //Turn right to face switch.
+    			addSequential(new PDriveToDistance(3.887 * RobotMap.Values.ticksPerFoot)); //Diagonal length towards our switch. Prev value: 5.5
+    			addSequential(new PDriveToAngle(-60)); //Turn left to face straight again.
+    			addSequential(new ElevatorToHeight(RobotMap.Values.elevatorSwitchHeight));
+    			addSequential(new PDriveToDistance((RobotMap.Values.autoRightSwitchTotal - 5.887) * RobotMap.Values.ticksPerFoot)); //Drive to reach switch for cube delivery. Prev value: 4.15
+    			//PRETTY ACCURATE NOW :^)
+    			//When our side of the switch is on the right, this will deliver the cube to
+    			//that side.
+    			
+    		}
+    		//addSequential(new ElevatorToHeight(RobotMap.Values.elevatorSwitchHeight));
+    		addSequential(new FlopDown());
+    		addSequential(new TimedUncollect(-0.5, -0.5, 3));
     	}
-    	else {
-    		addSequential(new PDriveToAngle(65)); //Turn right to face switch.
-    		addSequential(new PDriveToDistance(3.887 * RobotMap.Values.ticksPerFoot)); //Diagonal length towards our switch. Prev value: 5.5
-    		addSequential(new PDriveToAngle(-60)); //Turn left to face straight again.
-    		addSequential(new ElevatorToHeight(RobotMap.Values.elevatorSwitchHeight));
-    		addSequential(new PDriveToDistance((RobotMap.Values.autoRightSwitchTotal - 5.887) * RobotMap.Values.ticksPerFoot)); //Drive to reach switch for cube delivery. Prev value: 4.15
-    		//PRETTY ACCURATE NOW :^)
-    		//When our side of the switch is on the right, this will deliver the cube to
-    		//that side.
-    	
-    	}
-		//addSequential(new ElevatorToHeight(RobotMap.Values.elevatorSwitchHeight));
-    	addSequential(new FlopDown());
-		addSequential(new TimedUncollect(-0.5, -0.5, 3));
     }
 }
 
--- src/org/usfirst/frc/team997/robot/commands/SwitchSameSideDelivery.java
+++ src/org/usfirst/frc/team997/robot/commands/SwitchSameSideDelivery.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class SwitchSameSideDelivery extends CommandGroup {
 
     public SwitchSameSideDelivery() {
+        if(!Robot.getGameData().isEmpty()) {
         	
         	addSequential(new PDriveToDistance(14 * RobotMap.Values.ticksPerFoot));
         	if (Robot.getGameData().charAt(0) == 'L') {
@@ -19,6 +20,6 @@ public class SwitchSameSideDelivery extends CommandGroup {
         		addSequential(new PDriveToAngle(-90)); //Turn left to face switch.
         	}
         	//TODO How far does the robot have to travel to get close enough to the switch?
-
+        }
     }
 }
